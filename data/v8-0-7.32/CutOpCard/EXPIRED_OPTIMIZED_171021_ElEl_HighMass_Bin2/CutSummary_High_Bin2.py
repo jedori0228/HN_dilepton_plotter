22e84d537b60103b163186735b92cf5131048c66
Jae Sung Kim <jae.sung.kim@cern.ch>
2017-11-16 12:57:15 +0900

    expired cutop directory..

---
+++ data/v8-0-7.32/CutOpCard/EXPIRED_OPTIMIZED_171021_ElEl_HighMass_Bin2/CutSummary_High_Bin2.py
@@ -0,0 +1,79 @@
+import os
+
+DoBin1 = False
+
+masses = [90, 100, 125, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]
+
+highermasses = [250, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]
+
+varorder = [
+"leadingLepton_Pt >",
+"secondLepton_Pt >",
+"m_lljj_jjWclosest >",
+]
+names = [
+"pt1 >\t",
+"pt2 >\t",
+"m(lljj) >\t",
+]
+
+varsinboth = [
+"m_jj_jjWclosest >",
+"m_SubLeadljj_jjWclosest >"
+]
+
+print 'm(N) [GeV]\t',
+for name in names:
+  print name,
+print ""
+
+for mass in masses:
+  lines = open('HNElEl_'+str(mass)+'.txt').readlines()
+
+  print str(mass)+'\t',
+  counter = 0
+  for var in varorder:
+
+    for line in lines:
+      words = line.split()
+      this_var = words[0]+" "+words[1]
+      if not (this_var == var):
+        continue
+
+      value = str(int(float(words[2])))
+
+      if var in varsinboth:
+        print value+" -",
+      else:
+        print value+'\t',
+
+    if "secondLepton_Pt" in var:
+      print "40 - 150\t",
+
+    if "m_lljj_jjWclosest" in var:
+      print "\t15\t",
+
+    counter = counter+1
+
+
+  logname = 'Cycles/log_HNElEl_'+str(mass)+'.log'
+  if not DoBin1:
+    logname = 'Cycles/log_Bin2_HNElEl_'+str(mass)+'.log'
+
+  runlog = open(logname).readlines()
+  bkgd = runlog[len(runlog)-2].split()[0]
+  eff = runlog[len(runlog)-3].split()[2]
+  print bkgd+'\t'+eff
+
+
+
+
+
+
+
+
+
+
+
+
+
